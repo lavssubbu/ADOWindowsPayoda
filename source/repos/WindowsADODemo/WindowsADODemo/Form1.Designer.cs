16b32ede7367768f944e903179f6fb533442ad8c
Lavanya <lavssubbu@gmail.com>
2024-08-27 19:28:44 +0530

    Employee

---
+++ source/repos/WindowsADODemo/WindowsADODemo/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace WindowsADODemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSal = new System.Windows.Forms.TextBox();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtDept = new System.Windows.Forms.TextBox();
+            this.Insert = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btntot = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(263, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(487, 237);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(145, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Display";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "EmployeeID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "EmployeeName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Depatrment";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Salary";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "JoiningDate";
+            // 
+            // txtSal
+            // 
+            this.txtSal.Location = new System.Drawing.Point(145, 182);
+            this.txtSal.Name = "txtSal";
+            this.txtSal.Size = new System.Drawing.Size(100, 20);
+            this.txtSal.TabIndex = 7;
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(145, 31);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(100, 20);
+            this.txtid.TabIndex = 8;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(145, 78);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 20);
+            this.txtname.TabIndex = 9;
+            // 
+            // txtDept
+            // 
+            this.txtDept.Location = new System.Drawing.Point(145, 126);
+            this.txtDept.Name = "txtDept";
+            this.txtDept.Size = new System.Drawing.Size(100, 20);
+            this.txtDept.TabIndex = 10;
+           // this.txtDept.TextChanged += new System.EventHandler(this.txtDept_TextChanged);
+            // 
+            // Insert
+            // 
+            this.Insert.Location = new System.Drawing.Point(36, 335);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(75, 23);
+            this.Insert.TabIndex = 12;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = true;
+            this.Insert.Click += new System.EventHandler(this.Insert_Click);
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(263, 335);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(75, 23);
+            this.Update.TabIndex = 13;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(378, 335);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(484, 335);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 15;
+            this.btnFind.Text = "Search";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // btntot
+            // 
+            this.btntot.Location = new System.Drawing.Point(583, 335);
+            this.btntot.Name = "btntot";
+            this.btntot.Size = new System.Drawing.Size(167, 23);
+            this.btntot.TabIndex = 16;
+            this.btntot.Text = "TotalEmployees";
+            this.btntot.UseVisualStyleBackColor = true;
+            this.btntot.Click += new System.EventHandler(this.btntot_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(145, 239);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(112, 20);
+            this.dateTimePicker1.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btntot);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Insert);
+            this.Controls.Add(this.txtDept);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.txtSal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSal;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtDept;
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btntot;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ source/repos/WindowsADODemo/WindowsADODemo/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsADODemo
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader sdr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection("data source = LAPTOP-BQF0DTHQ\\SQLEXPRESS; database = samp2024; integrated security = true;");
+
+            con.Open();
+
+            cmd = new SqlCommand("select * from DoverEmp", con);
+
+            sdr = cmd.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(sdr);
+
+            dataGridView1.DataSource = dt;
+          
+            con.Close();
+        }
+
+        private void Insert_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection("data source = LAPTOP-BQF0DTHQ\\SQLEXPRESS; database = samp2024; integrated security = true;");
+
+            con.Open();
+            int eid = Convert.ToInt32(txtid.Text);
+            string name = txtname.Text;
+            string dpt = txtDept.Text;
+            int sal = Convert.ToInt32(txtSal.Text);
+            DateTime date = DateTime.Parse(dateTimePicker1.Text);
+            
+            cmd = new SqlCommand("insert into DoverEmp values(@eid,@name,@dpt,@sal,@date)", con);
+            cmd.Parameters.AddWithValue("@eid", eid);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@dpt", dpt);
+            cmd.Parameters.AddWithValue("@sal", sal);
+            cmd.Parameters.AddWithValue("@date", date);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("REcord Inserted");
+            con.Close();
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection("data source = LAPTOP-BQF0DTHQ\\SQLEXPRESS; database = samp2024; integrated security = true;");
+
+            con.Open();
+            int eid = Convert.ToInt32(txtid.Text);
+            string name = txtname.Text;
+            string dpt = txtDept.Text;
+            int sal = Convert.ToInt32(txtSal.Text);
+            DateTime date = DateTime.Parse(dateTimePicker1.Text);
+
+           cmd = new SqlCommand("update DoverEmp set Empname=@name,dept =@dpt,salary=@sal,JoiningDate=@date where Empid =@eid", con);
+            cmd.Parameters.AddWithValue("@eid", eid);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@dpt", dpt);
+            cmd.Parameters.AddWithValue("@sal", sal);
+            cmd.Parameters.AddWithValue("@date", date);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("REcord Updated");
+            con.Close();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection("data source = LAPTOP-BQF0DTHQ\\SQLEXPRESS; database = samp2024; integrated security = true;");
+            con.Open();
+            int eid = Convert.ToInt32(txtid.Text);
+            cmd = new SqlCommand("delete from DoverEmp where Empid =@eid", con);
+            cmd.Parameters.AddWithValue("@eid", eid);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("REcord Deleted");
+            con.Close();
+
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection("data source = LAPTOP-BQF0DTHQ\\SQLEXPRESS; database = samp2024; integrated security = true;");
+            con.Open();
+            // int eid = Convert.ToInt32(txtid.Text);
+           DateTime dat=Convert.ToDateTime(dateTimePicker1.Text);
+            cmd = new SqlCommand("select * from DoverEmp where month(joiningdate) =month(@date)", con);
+            cmd.Parameters.AddWithValue("@date", dat);
+            MessageBox.Show("One Product search");
+            sdr = cmd.ExecuteReader();
+         //   txtname.Text = sdr[].ToString();
+            DataTable dt = new DataTable();
+            dt.Load(sdr);
+            dataGridView1.DataSource = dt;
+            con.Close();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection("data source = LAPTOP-BQF0DTHQ\\SQLEXPRESS; database = samp2024; integrated security = true;");
+
+            con.Open();
+
+            cmd = new SqlCommand("select * from DoverEmp", con);
+
+            sdr = cmd.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(sdr);
+
+            dataGridView1.DataSource = dt;
+
+            con.Close();
+        }
+
+        private void btntot_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection("data source = LAPTOP-BQF0DTHQ\\SQLEXPRESS; database = samp2024; integrated security = true;");
+            con.Open();
+            cmd = new SqlCommand("select count(*) from DoverEmp", con);
+            int count = (int)cmd.ExecuteScalar();
+            MessageBox.Show($"Total No: of Employees: {count}");
+
+
+
+        }
+
+    }
+}
